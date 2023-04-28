f17fe3ab4d65c22dfed5572fc5894d845ad3534d
erlm <erlm@kea.dk>
2023-04-28 08:46:06 +0200

    Product service

--- src/main/java/com/example/vandeliveryv1/controller/ProductRESTController.java
+++ src/main/java/com/example/vandeliveryv1/controller/ProductRESTController.java
@@ -2,9 +2,11 @@ package com.example.vandeliveryv1.controller;
 
 import com.example.vandeliveryv1.model.Product;
 import com.example.vandeliveryv1.repository.ProductRepository;
+import com.example.vandeliveryv1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,11 +14,34 @@ import java.util.List;
 public class ProductRESTController {
 
     @Autowired
-    ProductRepository productRepository;
+    ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public List<Product> getAllProduct() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Integer id) {
+        Product product = productService.getProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product newProduct = productService.createProduct(product);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Integer id, @RequestBody Product product) {
+        return new ResponseEntity<>(productService.updateProduct(id, product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") Integer id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
